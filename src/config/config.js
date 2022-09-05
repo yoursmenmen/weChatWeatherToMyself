5ff481832f1e40506377bae93096b2edc551f53c
yoursmenmen <102282504+yoursmenmen@users.noreply.github.com>
2022-09-05 22:28:38 +0800

    差点泄露个人信息

--- src/config/config.js
+++ src/config/config.js
@@ -7,10 +7,10 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const params = {
-  appId: "wx6a93fcdb891146ff",
-  appSecret: "65640ad03a61f89cace1efb1be6a42df",
-  touser: "onSQq6-qJZvfsS-AgTb9GF-lM3fg",
-  template_id: "nlRqlLIP0eN4_j6B4EGVpW5U_ebH97Q5ucEZePTUkq8",
+  appId: "",
+  appSecret: "",
+  touser: "",
+  template_id: "",
 };
 
 module.exports = {
